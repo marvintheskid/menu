f9c0dbe479ca5c920b4f265809ca19cf32153b6a
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2020-09-19 14:15:04 +0200

    Created a SimpleMenu interface

---
+++ src/main/java/me/marvin/menu/impl/SimpleMenu.java
@@ -0,0 +1,55 @@
+package me.marvin.menu.impl;
+
+import me.marvin.menu.Menu;
+import me.marvin.menu.button.Button;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * A simple {@link Menu} implementation powered by functional suppliers.
+ *
+ * @author marvin
+ * @version 1.0
+ *
+ * @see Menu
+ */
+public class SimpleMenu extends Menu {
+    private final Function<Player, Map<Integer, Button>> buttonFunction;
+    private final Function<Player, String> titleFunction;
+    private final Function<Player, Integer> sizeFunction;
+
+    public SimpleMenu(Function<Player, Map<Integer, Button>> buttonFunction, Function<Player, String> titleFunction, Function<Player, Integer> sizeFunction) {
+        this.buttonFunction = buttonFunction;
+        this.titleFunction = titleFunction;
+        this.sizeFunction = sizeFunction;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @NotNull
+    public Map<Integer, Button> getButtons(@NotNull Player player) {
+        return buttonFunction.apply(player);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @NotNull
+    public String getMenuTitle(@NotNull Player player) {
+        return titleFunction.apply(player);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getSize(@NotNull Player player) {
+        return sizeFunction.apply(player);
+    }
+}
