27f67b46bdd1476e5517cdb680c2dd6042064227
marvintheskid <46824675+marvintheskid@users.noreply.github.com>
2020-09-19 14:14:40 +0200

    Added things to the ButtonEvent interface

--- src/main/java/me/marvin/menu/button/ButtonEvent.java
+++ src/main/java/me/marvin/menu/button/ButtonEvent.java
@@ -1,8 +1,11 @@
 package me.marvin.menu.button;
 
 import me.marvin.menu.listener.ButtonInteractionListener;
+import org.bukkit.event.Event;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 /**
@@ -20,10 +23,23 @@ public interface ButtonEvent extends Consumer<InventoryClickEvent> {
     /**
      * A simple {@link ButtonEvent} which cancels every interaction given towards the desired button
      */
-    ButtonEvent CANCELLING_EVENT = (evt) -> evt.setCancelled(true);
+    ButtonEvent CANCELLING_EVENT = (evt) -> {
+        evt.setCancelled(true);
+        evt.setResult(Event.Result.DENY);
+    };
 
     /**
      * A no-operation {@link ButtonEvent}, which does nothing.
      */
     ButtonEvent NOOP_EVENT = (evt) -> {};
+
+    @Override
+    @NotNull
+    default ButtonEvent andThen(@NotNull Consumer<? super InventoryClickEvent> after) {
+        Objects.requireNonNull(after);
+        return (t) -> {
+            accept(t);
+            after.accept(t);
+        };
+    }
 }
